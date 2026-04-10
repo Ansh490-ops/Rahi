5f65b04b4b69eedec88cc8bcb4a65ef5fd20b849
Rahi <anshsingh13122@gmail.com>
2026-04-10 06:51:01 +0530

    Updated line 13 in Script.js

--- Script.js
+++ Script.js
@@ -1,8 +1,8 @@
-// Your existing JavaScript code
-// Replace this comment with your existing code
-// Change made on 2026-04-10 01:19:52 UTC
+// Updated Script.js
 
-// ...
+// ... other code ...
 
-// Example placeholder text in line 13
-console.log("Note: No. of passengers, service duration ( like 8 AM to 2 PM)");
+// Change made here: line 13
+// Note: No. of passengers, service duration ( like 8 AM to 2 PM)
+
+// ... other code ...
